2c3ace02f0aeae6ebd41a3fd50d48b687d23e15d
Vaidehi Dubey <vaidehidubey282@gmail.com>
2026-02-15 20:59:19 +0530

    commit

---
+++ backend/models/post.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+const PostSchema =mongoose.Schema({
+    UserId:{
+
+    },
+    body:{
+       type:String,
+         required:true
+    },
+    likes:{
+        type:Number,
+        default:0
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    updatedAt:{
+        type:Date,
+        default:Date.now
+    },
+    media:{
+        type:String,
+        default:''
+    },
+    active:{
+        type:Boolean,
+        default:true
+    },
+    fileType:{
+        type:String,
+        default:''
+    },
+
+});
+const PostModel = mongoose.model("Post",PostSchema);
+export default PostModel;
