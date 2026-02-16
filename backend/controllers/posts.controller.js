a212e623753041c19eeb3f27a3903cdf13f80c9b
Vaidehi Dubey <vaidehidubey282@gmail.com>
2026-02-16 22:52:23 +0530

    controller

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -1,6 +1,25 @@
-
-
+import User from "../models/user.model.js";
 export const activeCheck = async(req , res) =>{
 
     return res.status(200).json({message: "RUNNING"})
+}
+
+const register = async(req , res) =>{
+
+
+    try{
+        const {name , email , password} = req.body;
+
+        if(!name || !email || !password ||!username){
+            return res.status(400).json({message: "Please fill all the fields"})
+        }
+
+        const user = await User.finOne({email
+        });
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({message: "Internal Server Error"})
+    }
+
 }
