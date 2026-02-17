755e1688818ca9e14bf6cde9cad1b75239b8933d
Vaidehi Dubey <vaidehidubey282@gmail.com>
2026-02-17 21:29:41 +0530

    initial

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,36 @@
+import Profile from "../models/profile.model.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+
+export const register = async(req , res) =>{
+
+
+    try{  
+        const {name , email , password} = req.body;
+
+        if(!name || !email || !password ||!username){
+            return res.status(400).json({message: "Please fill all the fields"})
+        }
+
+        const user = await User.finOne({email
+        });
+
+        const hashedPassword = await bcrypt.hash(password,10);
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword,
+            username
+        });
+        await newUser.save();
+        const profilr = new Profile({
+            userId: newUser._id,
+        });
+        return res.status(201).json({message: "User registered successfully"})
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({message: "Internal Server Error"})
+    }
+
+}
--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,17 @@ import cors from 'cors';
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import postsRoutes from "./routes/postsroute.js";
+import userRoutes from "./routes/userroutes.js";
 dotenv.config();
 const app = express();
 app.use(cors());
+
+
+app.use("/api/posts", postsRoutes);
+app.use("/api/users", userRoutes);
+
 app.use(express.json());
+
 const start =async ()=>{
     const connectDB= await mongoose.connect("mongodb+srv://vaidehidubey121_db_user:gauri122005@apnaconnection.omzhkwt.mongodb.net/?appName=ApnaConnection")
     app.listen(9090, ()=>{
