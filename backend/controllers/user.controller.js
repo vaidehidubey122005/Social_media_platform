7ae06a64bb3154bb00213d1effcb5b874f19cef9
Vaidehi Dubey <vaidehidubey282@gmail.com>
2026-02-19 22:45:56 +0530

    login

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -34,3 +34,25 @@ export const register = async(req , res) =>{
     }
 
 }
+
+export const login = async(req , res) =>{
+    try{
+        const {email , password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({message: "Please fill all the fields"})
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(404).json({message: "User does not exist"})
+        }                       
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(404).json({message: "Invalid credentials"})
+        }
+        return res.status(200).json({message: "Login successful"})
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({message: "Internal Server Error"})
+    }
+}
