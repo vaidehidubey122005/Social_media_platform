760e8cf66567647477ee9f2960bfef37e8304f0a
Vaidehi Dubey <vaidehidubey282@gmail.com>
2026-02-21 22:57:02 +0530

    database reconnection

--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -1,7 +1,10 @@
-import express from "express";
+import { Router } from "express";
 import { register, login } from "../controllers/user.controller.js";
 
-const router = express.Router();
+const router = Router();
+
+
+
 
 router.post("/register", register);
 router.post("/login", login);
