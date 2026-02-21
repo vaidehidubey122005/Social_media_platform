111720552816fa0dec875f7be015e040e3008c17
Vaidehi Dubey <vaidehidubey282@gmail.com>
2026-02-21 22:59:46 +0530

    .

--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -3,7 +3,14 @@ import { register, login } from "../controllers/user.controller.js";
 
 const router = Router();
 
-
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) { 
+        cb(null, 'profile_picture/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
 
 
 router.post("/register", register);
