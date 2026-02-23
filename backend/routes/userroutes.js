33bfe561ccdf929482e7d131792d9ca0d79c66a3
Vaidehi Dubey <vaidehidubey282@gmail.com>
2026-02-23 20:23:43 +0530

    commit

--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -1,19 +1,25 @@
 import { Router } from "express";
 import { register, login } from "../controllers/user.controller.js";
 import multer from "multer";
+import path from "path";
+
 const router = Router();
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) { 
-        cb(null, 'profile_picture/');
+    destination: function (req, file, cb) {
+        cb(null, path.resolve("profile_picture"));
     },
-    filename: function(req, file, cb) {
-        cb(null, file.originalname);
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`);
     }
 });
-const upload=multer({ storage: storage });
 
-router.post("/register", register);
+const upload = multer({ storage });
+
+
+router.route("/update_profile_picture")
+.post(upload.single("profile_picture"))
+router.post("/register", upload.single("profile_picture"), register);
 router.post("/login", login);
 
 export default router;
