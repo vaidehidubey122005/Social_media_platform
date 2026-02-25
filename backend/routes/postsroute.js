d191af3757b442d870dd7e54d2ac2fdbc3598d33
Vaidehi Dubey <vaidehidubey282@gmail.com>
2026-02-25 22:16:41 +0530

    commit

--- backend/routes/postsroute.js
+++ backend/routes/postsroute.js
@@ -1,8 +1,5 @@
 import {Router} from "express";
 import { activeCheck } from "../controllers/posts.controller.js";
-
 const router= Router();
-
 router.route('/').get(activeCheck);
-
 export default router;
