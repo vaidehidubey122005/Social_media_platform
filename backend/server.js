7c9e96f51a456658c5dacaac29c0caee462b5e64
Vaidehi Dubey <vaidehidubey282@gmail.com>
2026-03-03 22:13:41 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,12 @@ import postsRoutes from "./routes/postsroute.js";
 import userRoutes from "./routes/userroutes.js";
 dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/posts", postsRoutes);
 app.use("/api/users", userRoutes);
+
 const start =async ()=>{
     const connectDB= await mongoose.connect("mongodb+srv://vaidehidubey121_db_user:gauri122005@apnaconnection.omzhkwt.mongodb.net/?appName=ApnaConnection")
     app.listen(9090, ()=>{
