53ded0bd18c8365f6627b481a4e005f7f4e6c91a
Vaidehi Dubey <vaidehidubey282@gmail.com>
2026-03-08 22:52:03 +0530

    commit

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+
 export const activeCheck = async(req , res) =>{
     return res.status(200).json({message: "RUNNING"})
 }
